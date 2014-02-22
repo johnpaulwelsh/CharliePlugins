3fc028e8f0a7eb143bb25fe13451642d351606cd
John Paul Welsh <wearing2raincoats@yahoo.com>
2014-02-21 19:02:41 -0500

    first commit, sort of setup

---
+++ src/charlie/advisor/Advisor.java
@@ -0,0 +1,21 @@
+/**
+ * Our implementation of a Charlie blackjack advisor plugin.
+ */
+
+package charlie.advisor;
+
+import charlie.card.Card;
+import charlie.card.Hand;
+import charlie.plugin.IAdvisor;
+import charlie.util.Play;
+
+/**
+ * @author Katie Craven and John Paul Welsh
+ */
+public class Advisor implements IAdvisor {
+
+    @Override
+    public Play advise(Hand myHand, Card upCard) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }   
+}
